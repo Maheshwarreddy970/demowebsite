828ec9588e07924c5388b8362fec12a7abd328d0
Maheshwarreddy970 <mr8382455@gmail.com>
2025-03-07 13:55:57 +0530

    adding tailwindcsss

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,23 +1,12 @@
 'use client';
 
-import { useState } from "react";
 import { SidebarAdmin } from "@/components/admin/sidebar";
-import { SignupFormDemo } from "@/components/Signup";
 
 interface AppLayoutProps {
   children: React.ReactNode;
 }
 
 export default function AppLayout({ children }: AppLayoutProps) {
-  const [isAuthenticated, setIsAuthenticated] = useState(true);
- 
-  if (!isAuthenticated) {
-    return (
-      <div className="flex items-center justify-center h-screen">
-      <SignupFormDemo setIsAuthenticated={setIsAuthenticated}></SignupFormDemo>
-      </div>
-    );
-  }
 
   return (
     <main>
